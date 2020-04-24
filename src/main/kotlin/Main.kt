8e77d551c74697995e7135ea8590c265674564db
Megan Lov <mlov2@illinois.edu>
2020-04-24 16:08:56 -0500

    Added test case for POST request

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,10 @@ import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
 // import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
-import io.ktor.routing.post
 // import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
@@ -36,13 +34,13 @@ fun Application.userPage() {
             call.respondText(welcome() + " Please sign in.")
         }
         /////
-        post("/signin") {
-            val request = call.receive<SignIn>()
-            val user = request.username
-            call.respond(user)
-            // println(request)
-            // call.respond(HttpStatusCode.OK)
-        }
+        // post("/signin") {
+        //     val request = call.receive<SignIn>()
+        //     val user = request.username
+        //     call.respond(user)
+        //     println(request)
+        //     // call.respond(HttpStatusCode.OK)
+        // }
         /////
         get("/{user}/{page}") {
             try {
--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -4,6 +4,7 @@ import io.ktor.application.Application
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
 
 // class TestMain : StringSpec({
@@ -18,6 +19,22 @@ class TestMain : StringSpec({
             handleRequest(HttpMethod.Get, "/").apply {
                 response.status() shouldBe HttpStatusCode.OK
                 response.content shouldBe "Welcome! Please sign in."
+            }
+        }
+    }
+    "should accept post signin request" {
+        withTestApplication(Application::userPage) {
+            handleRequest(HttpMethod.Post, "/signin") {
+                addHeader("content-type", "application/json")
+                setBody("""
+                    {
+                        "username": "user",
+                        "password": "password",
+                        "name": "name"
+                    }
+                """.trim())
+            }.also {
+
             }
         }
     }
