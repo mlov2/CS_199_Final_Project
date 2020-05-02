6496d8bedc7f49a5150c707d1c50f078064f75f0
Megan Lov <mlov2@illinois.edu>
2020-05-01 20:28:08 -0500

    Incorporated Mustache in the Library page, tried getting buttons to work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
 // import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.response.respondText
+// import io.ktor.response.respondText
 import io.ktor.routing.get
 // import io.ktor.routing.post
 import io.ktor.routing.routing
@@ -41,23 +41,29 @@ fun Application.userPage() {
     // }
 
     // Mustache
-    install(Mustache) {
-        mustacheFactory = DefaultMustacheFactory("templates")
-    }
+        install(Mustache) {
+            mustacheFactory = DefaultMustacheFactory("templates")
+        }
 
     //routes
     routing {
         get("/") {
          //   val phrase = "my CS 199 Final Project"
          //   call.respond(FreeMarkerContent("Welcome.ftl", phrase))
-            call.respondText(
-                welcome() +
-                    "\n\n\nPlease sign in to continue." +
-                    "\nUsername: <TEXT FIELD TO TYPE USERNAME GOES HERE>" +
-                    "\nPassword: <TEXT FIELD TO TYPE PASSWORD GOES HERE>" +
-                    "\n<SIGN IN BUTTON GOES HERE>" +
-                    "\n\nDon't have an account?  Click here to sign up! <SIGN UP BUTTON GOES HERE>"
-            )
+
+            //With Mustache
+            val user = SignIn("username", "password", "user")
+            call.respond(MustacheContent("Welcome.html", mapOf("user" to user)))
+
+            //Without Mustache
+            // call.respondText(
+            //     welcome() +
+            //         "\n\n\nPlease sign in to continue." +
+            //         "\nUsername: <TEXT FIELD TO TYPE USERNAME GOES HERE>" +
+            //         "\nPassword: <TEXT FIELD TO TYPE PASSWORD GOES HERE>" +
+            //         "\n<SIGN IN BUTTON GOES HERE>" +
+            //         "\n\nDon't have an account?  Click here to sign up! <SIGN UP BUTTON GOES HERE>"
+            // )
         }
         /////
         // post("/signin") {
@@ -77,14 +83,28 @@ fun Application.userPage() {
                 when (page) {
                     "home" -> call.respond(MustacheContent("Home.html", mapOf("userDetails" to userDetails)))
                     "explore" -> call.respond(MustacheContent("Explore.html", mapOf("userDetails" to userDetails)))
-                    "library" -> call.respond(MustacheContent("Library.html", mapOf("userDetails" to userDetails)))
+                    "library" -> {
+                        if (currentlyReadingShelf.isNotEmpty()) {
+                            for ((book, author) in currentlyReadingShelf) {
+                                //val bookInfo = BookInfo(book, author)
+                                val books = LibraryInfo("Currently Reading Shelf", book, author, "")
+                                call.respond(MustacheContent("Library.html", mapOf("books" to books, "userDetails" to userDetails)))
+                                continue
+                            }
+                        } else {
+                            val emptyShelf = LibraryInfo("Currently Reading Shelf", "", "", "You aren't currently reading anything--click the Explore tab to start reading!")
+                            call.respond(MustacheContent("Library.html", mapOf("emptyShelf" to emptyShelf, "userDetails" to userDetails)))
+                        }
+                    }
+                    // "library" -> call.respond(MustacheContent("Library.html", mapOf("userDetails" to userDetails)))
                     "book buddies" -> call.respond(MustacheContent("BookBuddies.html", mapOf("userDetails" to userDetails)))
                 }
+
 
                 //without Mustache (original code)
                 // val user = call.parameters["user"]
                 // val page = call.parameters["page"]
-                val userPage = when (page) {
+/*                val userPage = when (page) {
                     // "home" -> "Welcome back, $user!"
                     // "explore" -> "These books might interest you, $user!"
                     "library" ->
@@ -104,7 +124,7 @@ fun Application.userPage() {
                         "$user's Book Buddies" +
                             "\nYou currently don't have any book buddies, but these users have read the same books as you--maybe they'll be your next book buddy! :)"
                     else -> throw Exception("The page '$page' does not exist")
-                }
+                }*/
                 // call.respond(userPage)
 
                 //the following can be used to return content in gson:
@@ -123,53 +143,66 @@ fun main() {
 }
 
 //lists for bookshelves
-val currentlyReadingShelf = mutableListOf("Moment of Truth", "Life As We Knew It")
-val wantToReadShelf = mutableListOf("Five Feet Apart")
-val recommendedShelf = mutableListOf("The Rest of The Story")
-val finishedShelf = mutableListOf("Legend", "Prodigy", "Champion", "Rebel")
-val favoritesShelf = mutableListOf("The Darkest Minds", "Never Fade", "In the Afterlight", "The Naturals", "")
+val currentlyReadingShelf = mapOf("Moment of Truth" to "Kasie West", "Life As We Knew It" to "Susan Beth Pfeffer")
+val wantToReadShelf = mapOf("Five Feet Apart" to "Rachael Lippincott")
+val recommendedShelf = mapOf("The Rest of The Story" to "Sarah Dessen")
+val finishedShelf = mapOf("Legend" to "Marie Lu", "Prodigy" to "Marie Lu", "Champion" to "Marie Lu", "Rebel" to "Marie Lu")
+val favoritesShelf = mapOf("The Darkest Minds" to "Alexandra Bracken", "Never Fade" to "Alexandra Bracken", "In the Afterlight" to "Alexandra Bracken", "The Naturals" to "Jennifer Lynn Barnes")
 val shelves = mutableListOf(currentlyReadingShelf, wantToReadShelf, recommendedShelf, finishedShelf, favoritesShelf)
-val cRAuthor = mutableListOf("Kasie West", "Susan Beth Pfeffer")
-val wTRAuthor = mutableListOf("Rachael Lippincott")
-val fAuthor = mutableListOf("Sarah Dessen")
-val favAuthor = mutableListOf("Marie Lu", "Marie Lu", "Marie Lu", "Marie Lu")
-val authors = mutableListOf(cRAuthor, wTRAuthor, fAuthor, favAuthor)
 
-fun shelves() {
-    for (shelf in shelves) {
-        if (shelf.size != 0) {
-            for (book in shelf) {
-                val bookInfo = BookInfo(book, "author")
-                val books = when (shelf) {
-                    currentlyReadingShelf -> LibraryInfo("Currently Reading Shelf", bookInfo.title, bookInfo.author, "")
-                    wantToReadShelf -> LibraryInfo("Want to Read", bookInfo.title, bookInfo.author, "")
-                    recommendedShelf -> LibraryInfo("Books Recommended By Book Buddies", bookInfo.title, bookInfo.author, "")
-                    finishedShelf -> LibraryInfo("Finished Reading", bookInfo.title, bookInfo.author, "")
-                    favoritesShelf -> LibraryInfo("megan's Favorites <3", bookInfo.title, bookInfo.author, "")
-                    else -> throw Exception("Um...there's a problem")
-                }
-                call.respond(MustacheContent("Library.html", mapOf("books" to books)))
-            }
-            continue
-        } else {
-            val emptyShelf = when (shelf) {
-                currentlyReadingShelf -> LibraryInfo("Currently Reading Shelf", "", "", "You aren't currently reading anything--click the Explore tab to start reading!")
-                wantToReadShelf -> LibraryInfo("Want to Read", "", "", "This shelf is currently empty--click the Explore tab to add books to this shelf!")
-                recommendedShelf -> LibraryInfo("Books Recommended By Your Book Buddies", "", "","You currently don't have any recommended books :(")
-                finishedShelf -> LibraryInfo("Finished Reading", "", "","You haven't finished any books yet :(")
-                favoritesShelf -> LibraryInfo("megan's Favorites", "", "","You don't have any favorite books yet, but don't worry!  You'll find one you really love!")
-                else -> throw Exception("Um...there's a problem")
-            }
-            MustacheContent("Library.html", mapOf("emptyShelf" to emptyShelf))
-        }
-    }
-    // "SORRY!  The site owner hasn't gotten to this part to work yet!"
-    // "ERROR!  Sorry, it seems like this isn't working correctly"
-}
+//DOESN'T WORK
+// fun Application.currentlyReadingShelf() {
+//     install(Mustache) {
+//         mustacheFactory = DefaultMustacheFactory("templates")
+//     }
+//
+//     if (currentlyReadingShelf.isNotEmpty()) {
+//         for ((book, author) in currentlyReadingShelf) {
+//             //val bookInfo = BookInfo(book, author)
+//             val books = LibraryInfo("Currently Reading Shelf", book, author, "")
+//             MustacheContent("Library.html", mapOf("books" to books))
+//             continue
+//         }
+//     } else {
+//         val emptyShelf = LibraryInfo("Currently Reading Shelf", "", "", "You aren't currently reading anything--click the Explore tab to start reading!")
+//         MustacheContent("Library.html", mapOf("emptyShelf" to emptyShelf))
+//     }
+// }
+
+// fun shelves() {
+//     for (shelf in shelves) {
+//         if (shelf.size != 0) {
+//             for (book in shelf) {
+//                 val bookInfo = BookInfo(book, "author")
+//                 val books = when (shelf) {
+//                     currentlyReadingShelf -> LibraryInfo("Currently Reading Shelf", bookInfo.title, bookInfo.author, "")
+//                     wantToReadShelf -> LibraryInfo("Want to Read", bookInfo.title, bookInfo.author, "")
+//                     recommendedShelf -> LibraryInfo("Books Recommended By Book Buddies", bookInfo.title, bookInfo.author, "")
+//                     finishedShelf -> LibraryInfo("Finished Reading", bookInfo.title, bookInfo.author, "")
+//                     favoritesShelf -> LibraryInfo("megan's Favorites <3", bookInfo.title, bookInfo.author, "")
+//                     else -> throw Exception("Um...there's a problem")
+//                 }
+//                 MustacheContent("Library.html", mapOf("books" to books))
+//             }
+//             continue
+//         } else {
+//             val emptyShelf = when (shelf) {
+//                 currentlyReadingShelf -> LibraryInfo("Currently Reading Shelf", "", "", "You aren't currently reading anything--click the Explore tab to start reading!")
+//                 wantToReadShelf -> LibraryInfo("Want to Read", "", "", "This shelf is currently empty--click the Explore tab to add books to this shelf!")
+//                 recommendedShelf -> LibraryInfo("Books Recommended By Your Book Buddies", "", "","You currently don't have any recommended books :(")
+//                 finishedShelf -> LibraryInfo("Finished Reading", "", "","You haven't finished any books yet :(")
+//                 favoritesShelf -> LibraryInfo("megan's Favorites", "", "","You don't have any favorite books yet, but don't worry!  You'll find one you really love!")
+//                 else -> throw Exception("Um...there's a problem")
+//             }
+//             MustacheContent("Library.html", mapOf("emptyShelf" to emptyShelf))
+//         }
+//     }
+//     // "SORRY!  The site owner hasn't gotten to this part to work yet!"
+//     // "ERROR!  Sorry, it seems like this isn't working correctly"
+// }
 
 /**********************************************************************************************************************/
 //I STILL NEED TO WORK ON THE FUNCTIONS BELOW--REQUIRES THE FRONTEND TO WORK TOO
-
 
 //function that'll go into button - so if a button is clicked, this function will add the selected book onto the Want to Read shelf
 // fun addBook() {
